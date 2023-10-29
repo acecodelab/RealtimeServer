a29a3edb3fa4cd62c66c53bd221253086fa62477
Rajat Thakur <rajat.thakur@nxtgenvirtue.com>
2023-10-29 18:59:43 +0530

    OCt 29 2023

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,42 @@
 var express = require('express');
 var router = express.Router();
 var s = require('../webViewServer');
+const testFolder = './public';
+const fs = require('fs');
+var path = require('path');
 
-router.post('/command/:cardId', function(req, res) {
+
+router.post('/command/:cardId', function (req, res) {
     var cardId = req.params.cardId;
     var data = req.body;
 
     console.log('hI hERE', data);
-    s.send(cardId, data, function(err, receivedData){
-        if(err){
+    s.send(cardId, data, function (err, receivedData) {
+        if (err) {
             res.status(500).send(err.message)
-			console.error(new Date(),cardId, err);
-        }else
+            console.error(new Date(), cardId, err);
+        } else
             res.json(receivedData)
     });
 });
 
 
-router.post('/',function(req,res){
-console.log(req.body,'Sensor data');
+router.post('/', function (req, res) {
+    console.log(req.body, 'Sensor data');
+})
+
+router.get('/getUploadData', function (req, res) {
+    var imageListName=[];
+    fs.readdir(path.join(__dirname, '../public'), function (err, images) {
+        if (err) {
+            return console.error(err)
+        }
+        images.forEach(file => {
+            imageListName.push(file)
+        });
+        res.send(imageListName)
+    })
+
 })
 
 module.exports = router;
