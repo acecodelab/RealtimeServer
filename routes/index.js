303b25cea03157448e24268e5193d395273dbfef
Rajat Thakur <rajat.thakur@nxtgenvirtue.com>
2023-11-14 18:03:48 +0530

    Nov 14 2023

--- routes/index.js
+++ routes/index.js
@@ -171,7 +171,7 @@ router.post('/upload', upload.single('file'), async (req, res) => {
         console.log(from + '-----' + to)
 
         const queryText = 'INSERT INTO public.adv(name, animation, "from", "to", status,duration)VALUES (' + "'" + req.file.originalname + "'" + ', ' + "'" + req.body.transactionResult + "'" + ', ' + "'timezone('UTC'," + from + ")'" + ',  ' + "'timezone('UTC'," + to + ")'" + ',' + "'Y'" + ',  ' + "'" + req.body.duration + "'" + ' );';
-
+        console.log('INSERT INTO public.adv(name, animation, "from", "to", status,duration)VALUES (' + "'" + req.file.originalname + "'" + ', ' + "'" + req.body.transactionResult + "'" + ', ' + "'timezone('UTC'," + from + ")'" + ',  ' + "'timezone('UTC'," + to + ")'" + ',' + "'Y'" + ',  ' + "'" + req.body.duration + "'" + ' )')
         pool.query(queryText)
             .then((result) => {
                 res.send('File uploaded!');
